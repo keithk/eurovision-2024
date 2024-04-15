7ffc11f53c12925e63cca13d3a2c186ef2535172
keithkurson <keith@keithkurson.net>
2024-04-15 14:10:26 -0700

    Add my tracking for now

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 
 export default {
-  // Feel free to override these, they are pulling from package.json for default values.
   title: "keith.is | Eurovision 2024",
   description: "" || pkg.description,
-  language: "en"
+  language: "en",
+  tracking: true // Disable this if you're not me lol
 };
